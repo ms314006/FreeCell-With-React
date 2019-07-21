b16c3d9dabfbff31588a43c0f6260e427d1188e9
GQSM <ms314006@hotmail.com>
2019-07-21 15:45:45 +0800

    [CHANGE] 調成簡單版的題目

--- src/component/GameTable/GameTable.js
+++ src/component/GameTable/GameTable.js
@@ -6,6 +6,7 @@ import styles from './index.scss';
 
 const GameTable = (props) => {
   const {} = props;
+  /*
   const [questionLayout, setQuestionLayout] = useState([
     ['heart_2', 'club_1', 'heart_9', 'heart_1', 'diamond_2', 'spades_7', 'club_10'],
     ['spades_13', 'diamond_1', 'diamond_11', 'spades_3', 'diamond_4'],
@@ -15,6 +16,17 @@ const GameTable = (props) => {
     ['heart_8', 'heart_4', 'diamond_3', 'club_7', 'club_8', 'spades_2'],
     ['diamond_9', 'diamond_7', 'heart_3', 'heart_11', 'spades_5', 'heart_13'],
     ['diamond_10', 'spades_10', 'heart_6', 'heart_7', 'spades_12', 'spades_8']
+  ]);
+  */
+  const [questionLayout, setQuestionLayout] = useState([
+    ['heart_9', 'spades_7', 'club_10', 'diamond_2', 'heart_2', 'heart_1', 'club_1'],
+    ['spades_13', 'diamond_11', 'diamond_4', 'spades_3', 'diamond_1'],
+    ['club_12', 'club_11', 'diamond_8', 'spades_6', 'spades_4', 'heart_12', 'club_3'],
+    ['spades_9', 'heart_10', 'club_6', 'club_9', 'club_4', 'diamond_5', 'club_2'],
+    ['club_5', 'diamond_13', 'heart_5', 'diamond_6', 'club_13', 'diamond_12', 'spades_11', 'spades_1'],
+    ['heart_8', 'club_7', 'club_8', 'heart_4', 'diamond_3', 'spades_2'],
+    ['diamond_9', 'diamond_7', 'heart_11', 'heart_13', 'spades_5', 'heart_3'],
+    ['diamond_10', 'spades_10', 'spades_12', 'spades_8', 'heart_7', 'heart_6']
   ]);
   useEffect(() => {
     const questionEmptyColumnCount = questionLayout.reduce((count, value) => value.length === 0 ? count += 1 : count += 0, 0);
@@ -30,14 +42,14 @@ const GameTable = (props) => {
   const [operationRecord, setOperationRecord] = useState([{
     recordNumber: 0,
     questionLayout: [
-      ['heart_2', 'club_1', 'heart_9', 'heart_1', 'diamond_2', 'spades_7', 'club_10'],
-      ['spades_13', 'diamond_1', 'diamond_11', 'spades_3', 'diamond_4'],
+      ['heart_9', 'spades_7', 'club_10', 'diamond_2', 'heart_2', 'heart_1', 'club_1'],
+      ['spades_13', 'diamond_11', 'diamond_4', 'spades_3', 'diamond_1'],
       ['club_12', 'club_11', 'diamond_8', 'spades_6', 'spades_4', 'heart_12', 'club_3'],
-      ['club_4', 'spades_9', 'heart_10', 'club_6', 'diamond_5', 'club_9', 'club_2'],
-      ['club_5', 'diamond_13', 'heart_5', 'spades_1', 'diamond_6', 'club_13', 'diamond_12', 'spades_11'],
-      ['heart_8', 'heart_4', 'diamond_3', 'club_7', 'club_8', 'spades_2'],
-      ['diamond_9', 'diamond_7', 'heart_3', 'heart_11', 'spades_5', 'heart_13'],
-      ['diamond_10', 'spades_10', 'heart_6', 'heart_7', 'spades_12', 'spades_8']
+      ['spades_9', 'heart_10', 'club_6', 'club_9', 'club_4', 'diamond_5', 'club_2'],
+      ['club_5', 'diamond_13', 'heart_5', 'diamond_6', 'club_13', 'diamond_12', 'spades_11', 'spades_1'],
+      ['heart_8', 'club_7', 'club_8', 'heart_4', 'diamond_3', 'spades_2'],
+      ['diamond_9', 'diamond_7', 'heart_11', 'heart_13', 'spades_5', 'heart_3'],
+      ['diamond_10', 'spades_10', 'spades_12', 'spades_8', 'heart_7', 'heart_6']
     ],
     tempLayout: [[], [], [], []],
     overLayout: [[], [], [], []],
